449d305f3af604ce8c60919582b9bd5d782de235
Kevin Krato <kratok08@gmail.com>
2019-04-16 20:52:32 -0600

    Modify file

--- src/components/Button.js
+++ src/components/Button.js
@@ -9,7 +9,7 @@ class Button extends React.Component {
     return (
       <button
         onClick={e => this.props.handleAnswer(e, this.props.name === "Got it!")}
-        disabled={!this.props.submitStatus}
+        // disabled={!this.props.submitStatus}
         className="btn-2"
       >
         {this.props.name}
