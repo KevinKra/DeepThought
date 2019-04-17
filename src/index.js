5cee8b4b4fde5adcaa4b11f41fa1b6cc50de6363
Kevin Krato <kratok08@gmail.com>
2019-04-16 20:46:38 -0600

    Remove DataSet import and default store state

--- src/index.js
+++ src/index.js
@@ -3,10 +3,8 @@ import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
 import "./styles/index.scss";
 import Router from "./components/Router";
-import DataSet from "./DataSet";
 import { combineReducers, createStore } from "redux";
 import { Provider } from "react-redux";
-
 import cardsReducer from "./redux/reducers/cards-reducer";
 import userReducer from "./redux/reducers/user-reducer";
 //import combineReducers redux method
@@ -18,23 +16,12 @@ const allReducers = combineReducers({
 //these are our default values that we are passing in at the start
 const store = createStore(
   allReducers,
-  {
-    cards: DataSet,
-    user: "steven"
-  },
+
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 ////////--CREATING AN ACTION w/ type & payload, dispatching the action to the reducers, who then update the store accordingly.
 // console.log(store.getState());
-
-// //user action with type of "updateUser" and a payload object 'user: "Jonnas"';
-// const updateUserAction = {
-//   type: "updateUser",
-//   payload: {
-//     user: "Jonnas"
-//   }
-// };
 
 // store.dispatch(updateUserAction);
 ////////
