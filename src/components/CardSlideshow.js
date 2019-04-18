ac95aaffb181101dce4c8111162d18359bea02da
Kevin Krato <kratok08@gmail.com>
2019-04-18 07:07:46 -0600

    Pass renderReview | renderNotFound method props

--- src/components/CardSlideshow.js
+++ src/components/CardSlideshow.js
@@ -3,38 +3,49 @@ import "../styles/_CardSlideshow.scss";
 
 import CardTopic from "./CardTopic";
 class CardSlideshow extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
   render() {
     return (
       <section className="card-slide-show">
         <CardTopic
           topic="CSS"
           blurb="Styles fundamentals"
-          history={this.props.history}
+          renderReview={this.props.renderReview}
+          renderNotFound={this.props.renderNotFound}
         />
         <CardTopic
           topic="HTML"
           blurb="<p>Learn Tags!</p>"
-          history={this.props.history}
+          renderReview={this.props.renderReview}
+          renderNotFound={this.props.renderNotFound}
         />
         <CardTopic
           topic="Functions"
           blurb="Check out these functions."
-          history={this.props.history}
+          renderReview={this.props.renderReview}
+          renderNotFound={this.props.renderNotFound}
         />
         <CardTopic
           topic="OOP"
           blurb="Object Oriented Guide"
-          history={this.props.history}
-        />
-        <CardTopic
-          topic="JavaScript"
-          blurb="Learn JavaScript!"
-          history={this.props.history}
+          renderReview={this.props.renderReview}
+          renderNotFound={this.props.renderNotFound}
         />
         <CardTopic
           topic="React"
           blurb="React Foundations"
-          history={this.props.history}
+          renderReview={this.props.renderReview}
+          renderNotFound={this.props.renderNotFound}
+        />
+        <CardTopic
+          topic="JavaScript"
+          blurb="Learn JavaScript!"
+          renderReview={this.props.renderReview}
+          renderNotFound={this.props.renderNotFound}
         />
         <div className="blur-screen" />
       </section>
