3b9a8b8de5b52259f4bc4efcfea6be0241a8c89d
Kevin Krato <kratok08@gmail.com>
2019-04-18 07:09:24 -0600

    Set state and conditional render

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -2,28 +2,83 @@ import React, { Component } from "react";
 import CardSlideshow from "./CardSlideshow";
 import NavBar from "./NavBar";
 import UserSection from "./UserSection";
+import ReviewPage from "./ReviewPage";
+import NotFound from "./NotFound";
 import "../styles/_MainPage.scss";
-import { connect } from "react-redux";
 
 class MainPage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      displayReviewPage: false,
+      displayNotFoundPage: false,
+      displayMainPage: true
+    };
+  }
+  componentDidMount() {
+    fetch(
+      "https://fe-apps.herokuapp.com/api/v1/memoize/1901/kevinkra/topicreact"
+    )
+      .then(res => res.json())
+      .then(data => console.log(data))
+      .then(data => this.props.onCollectAllCards(data))
+      .catch(err => console.log("ERROR ", err));
+  }
+
+  renderNotFoundPage = () => {
+    this.setState({
+      displayReviewPage: false,
+      displayNotFoundPage: true,
+      displayMainPage: false
+    });
+  };
+
+  renderReviewPage = () => {
+    this.setState({
+      displayReviewPage: true,
+      displayNotFoundPage: false,
+      displayMainPage: false
+    });
+  };
+
+  renderMainPage = () => {
+    this.setState({
+      displayReviewPage: false,
+      displayNotFoundPage: false,
+      displayMainPage: true
+    });
+  };
+
   render() {
-    //retrieve props from state tree and pass into user section;
-    console.log("username:", this.props.user);
-    return (
-      <main className="mainPage">
-        <NavBar history={this.props.history} />
-        <CardSlideshow history={this.props.history} />
-        <UserSection userName={this.props.user} />
-      </main>
+    const mainPage = (
+      <div className="mainPage">
+        <NavBar renderLoginPage={this.props.returnToLogin} />
+        <CardSlideshow
+          renderReview={this.renderReviewPage}
+          renderNotFound={this.renderNotFoundPage}
+        />
+        <UserSection userName={this.props.userName} />
+      </div>
     );
+    const renderPath = () => {
+      if (this.state.displayReviewPage) {
+        return (
+          <ReviewPage
+            renderLoginPage={this.props.returnToLogin}
+            renderMainPage={this.renderMainPage}
+          />
+        );
+      } else if (this.state.displayNotFoundPage) {
+        return <NotFound />;
+      } else if (this.state.displayMainPage) {
+        return mainPage;
+      } else {
+        alert("Perhaps return to login page if possible?");
+      }
+    };
+
+    return <React.Fragment>{renderPath()}</React.Fragment>;
   }
 }
 
-const mapStateToProps = state => ({
-  user: state.user
-});
-
-export default connect(
-  mapStateToProps
-  // mapActionsToProps
-)(MainPage);
+export default MainPage;
