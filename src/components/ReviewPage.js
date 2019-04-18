1ca43ed408cf394006c2fc296a41c322f51e99ce
Kevin Krato <kratok08@gmail.com>
2019-04-18 07:11:59 -0600

    Handle render path

--- src/components/ReviewPage.js
+++ src/components/ReviewPage.js
@@ -3,9 +3,6 @@ import NavBar from "./NavBar";
 import "../styles/_ReviewPage.scss";
 import ContentCard from "./ContentCard";
 import Prompt from "./Prompt";
-import { connect } from "react-redux";
-import { updateAllCards } from "../redux/actions/card-actions";
-import { collectAllCards } from "../redux/actions/card-actions";
 
 class ReviewPage extends React.Component {
   constructor(props) {
@@ -26,19 +23,9 @@ class ReviewPage extends React.Component {
 
   toMain = e => {
     e.preventDefault();
-    this.props.history.push(`/main/1`);
   };
 
-  resetList = () => {
-    const order = this.props.cards.TopicReact.indexOf(
-      this.props.cards.TopicReact.find(card => {
-        return !card.understood;
-      })
-    );
-    this.setState({
-      order: order
-    });
-  };
+  resetList = () => {};
 
   handleClick = e => {
     e.preventDefault();
@@ -47,75 +34,42 @@ class ReviewPage extends React.Component {
     });
   };
 
-  resetList = () => {
-    const order = this.props.cards.TopicReact.indexOf(
-      this.props.cards.TopicReact.find(card => {
-        return !card.understood;
-      })
-    );
-    this.setState({
-      order: order
-    });
-  };
+  resetList = () => {};
 
-  nextQuestion() {
-    const { TopicReact } = this.props.cards;
-    const order = TopicReact.indexOf(
-      TopicReact.find(
-        (card, i) => this.state.order + 1 <= i && !card.understood
-      )
-    );
-    this.setState({
-      order: order,
-      hasBeenClicked: false
-    });
-  }
+  nextQuestion() {}
 
-  handleAnswerResponse = (e, response) => {
+  handleAnswerResponse = e => {
     e.preventDefault();
-    const cards = [...this.props.cards.TopicReact];
-    cards[this.state.order].understood = response;
-    this.props.updateAllCards(cards);
-
-    if (this.props.cards.TopicReact.every(card => card.understood)) {
-      return;
-    }
-    this.state.order + 1 === this.props.cards.TopicReact.length
-      ? this.resetList()
-      : this.nextQuestion();
   };
 
   render() {
-    if (!this.props.cards.TopicReact) {
-      return null;
-    }
-
     if (!this.state.restartPrompt) {
     }
-
-    const currentCard = this.props.cards.TopicReact[this.state.order];
     return (
       <main className="window-reviewPage">
-        <NavBar history={this.props.history} mainLink={true} />
+        <NavBar
+          renderLoginPage={this.props.returnToLogin}
+          renderMainPage={this.props.renderMainPage}
+          mainLink={true}
+        />
         <section className="content-section">
           {!this.state.restartPrompt ? (
             <React.Fragment>
               <ContentCard
                 title="Question"
                 handleClick={this.handleClick}
-                text={currentCard.question}
+                // text={currentCard.question}
               />
               <ContentCard
                 title="Answer"
                 submitStatus={this.state.hasBeenClicked}
                 handleAnswer={this.handleAnswerResponse}
                 detectOrderNum={this.detectOrderNum}
-                text={currentCard.answer}
+                // text={currentCard.answer}
               />
             </React.Fragment>
           ) : (
             <Prompt
-              history={this.props.history}
               restartPrompt={this.state.restartPrompt}
               toMain={this.toMain}
             />
@@ -126,17 +80,4 @@ class ReviewPage extends React.Component {
   }
 }
 
-const mapStateToProps = state => ({
-  cards: state.cards
-});
-
-const mapActionsToProps = {
-  collectAllCards: collectAllCards,
-  updateAllCards: updateAllCards
-};
-
-//higher order component
-export default connect(
-  mapStateToProps,
-  mapActionsToProps
-)(ReviewPage);
+export default ReviewPage;
