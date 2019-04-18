2f44f27f1936299d96b8b10ed00d8e4c96336881
Kevin Krato <kratok08@gmail.com>
2019-04-18 17:22:26 -0600

    Add exit and reset functionality

--- src/components/Prompt.js
+++ src/components/Prompt.js
@@ -2,13 +2,20 @@ import React from "react";
 import "../styles/_Prompt.scss";
 
 class Prompt extends React.Component {
+  clearAndMainPage = () => {
+    localStorage.clear();
+    this.props.renderMainPage();
+  };
   render() {
     return (
       <article className="content-card">
         <h1>All cards reviewed!</h1>
         <div className="button-container">
           <button className="btn-2" onClick={this.props.renderMainPage}>
-            Exit
+            Exit and Save
+          </button>
+          <button className="btn-2" onClick={this.clearAndMainPage}>
+            Exit and Clear
           </button>
         </div>
       </article>
