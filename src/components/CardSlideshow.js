93f712a0cf48298dec64b46a0b6804a644e34934
Kevin Krato <kratok08@gmail.com>
2019-04-22 12:28:38 -0600

    Remove redundant cards

--- src/components/CardSlideshow.js
+++ src/components/CardSlideshow.js
@@ -12,38 +12,8 @@ class CardSlideshow extends React.Component {
     return (
       <section className="card-slide-show">
         <CardTopic
-          topic="CSS"
-          blurb="Styles fundamentals"
-          renderReview={this.props.renderReview}
-          renderNotFound={this.props.renderNotFound}
-        />
-        <CardTopic
-          topic="HTML"
-          blurb="<p>Learn Tags!</p>"
-          renderReview={this.props.renderReview}
-          renderNotFound={this.props.renderNotFound}
-        />
-        <CardTopic
-          topic="Functions"
-          blurb="Check out these functions."
-          renderReview={this.props.renderReview}
-          renderNotFound={this.props.renderNotFound}
-        />
-        <CardTopic
-          topic="OOP"
-          blurb="Object Oriented Guide"
-          renderReview={this.props.renderReview}
-          renderNotFound={this.props.renderNotFound}
-        />
-        <CardTopic
           topic="React"
           blurb="React Foundations"
-          renderReview={this.props.renderReview}
-          renderNotFound={this.props.renderNotFound}
-        />
-        <CardTopic
-          topic="JavaScript"
-          blurb="Learn JavaScript!"
           renderReview={this.props.renderReview}
           renderNotFound={this.props.renderNotFound}
         />
